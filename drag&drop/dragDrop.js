eafd13769700b4770da6934558141025eb68a762
Dominskyi <104265819+DenysDominskyi@users.noreply.github.com>
2022-06-20 15:16:24 +0300

    drag&drop

---
+++ drag&drop/dragDrop.js
@@ -0,0 +1,90 @@
+const gragField = document.querySelector('.drag-field');
+const gragItem = document.querySelector('.drag-field__item');
+
+gragItem.addEventListener("mousedown", function (event) {
+
+	let coordsItemX = event.clientX - gragItem.getBoundingClientRect().left;
+	let coordsItemY = event.clientY - gragItem.getBoundingClientRect().top;
+
+	let gragItemSizes = {
+		width: gragItem.offsetWidth,
+		height: gragItem.offsetHeight
+	}
+	let gragFieldSizes = {
+		left: gragField.getBoundingClientRect().left + scrollX,
+		top: gragField.getBoundingClientRect().top + scrollY,
+		right: gragField.getBoundingClientRect().left + scrollX + gragField.offsetWidth,
+		bottom: gragField.getBoundingClientRect().top + scrollY + gragField.offsetHeight
+	}
+
+	gragItem.style.position = 'absolute';
+	gragItem.style.zIndex = 1000;
+	document.body.append(gragItem);
+
+	moveItem(event.pageX, event.pageY);
+
+	function moveItem(pageX, pageY) {
+		let currentX = pageX - coordsItemX;
+		let currentY = pageY - coordsItemY;
+
+		if (
+			currentX + gragItemSizes.width <= gragFieldSizes.right &&
+			currentX >= gragFieldSizes.left
+		) {
+			gragItem.style.left = `${currentX}px`;
+		} else {
+			if (currentX + gragItemSizes.width > gragFieldSizes.right) {
+				gragItem.style.left = `${gragFieldSizes.right - gragItemSizes.width}px`;
+			}
+			if (currentX < gragFieldSizes.left) {
+				gragItem.style.left = `${gragFieldSizes.left}px`;
+			}
+		}
+		if (
+			currentY + gragItemSizes.height <= gragFieldSizes.bottom &&
+			currentY >= gragFieldSizes.top
+		) {
+			gragItem.style.top = `${currentY}px`;
+		} else {
+			if (currentY + gragItemSizes.height > gragFieldSizes.bottom) {
+				gragItem.style.top = `${gragFieldSizes.bottom - gragItemSizes.height}px`;
+			}
+			if (currentY < gragFieldSizes.top) {
+				gragItem.style.top = `${gragFieldSizes.top}px`;
+			}
+		}
+	}
+
+	let currentDroppable = null;
+
+	function onDragItem(event) {
+		moveItem(event.pageX, event.pageY);
+
+		gragItem.hidden = true;
+		let elemBelow = document.elementFromPoint(event.clientX, event.clientY);
+		gragItem.hidden = false;
+
+		if (!elemBelow) return;
+		let droppableBelow = elemBelow.closest('.drag-field__point');
+
+		if (currentDroppable !== droppableBelow) {
+			if (currentDroppable) {
+				currentDroppable.classList.remove('_active');
+				gragItem.classList.remove('_active');
+			}
+			currentDroppable = droppableBelow;
+			if (currentDroppable) {
+				currentDroppable.classList.add('_active');
+				gragItem.classList.add('_active');
+			}
+		}
+	}
+	document.addEventListener('mousemove', onDragItem);
+
+	document.addEventListener("mouseup", function (event) {
+		document.removeEventListener('mousemove', onDragItem);
+	}, { "once": true });
+});
+gragItem.addEventListener("dragstart", function (event) {
+	event.preventDefault();
+});
